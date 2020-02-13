449c1436017c5e1e6f3ed7948f30a4114bdd96dc
Karthikeyan <karthikeyan.live@hotmail.com>
2020-02-13 18:09:38 +0530

    Signup base added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ReactGA from "react-ga";
+import Firebase, { FirebaseContext } from "./common/components/Firebase";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import { App } from "./App";
 import "./index.css";
@@ -8,4 +9,9 @@ import "./index.css";
 ReactGA.initialize(process.env.REACT_APP_GA || "UA-00000-1");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById("root")
+);
