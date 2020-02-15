3685e0d2112848c9934f656ded836c78fce1fe96
karthikeyan-live <karthikeyan.live@hotmail.com>
2020-02-15 23:18:09 +0530

    SignUp page updated with Material theme

--- src/account/signup/index.js
+++ src/account/signup/index.js
@@ -1,13 +1,48 @@
 import React, { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import { withFirebase } from "../../common/components/Firebase";
 
-const SignUpPage = () => (
-  <div>
-    <h1>SignUp</h1>
-    <SignUpForm />
-  </div>
-);
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "} {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
 
 const INITIAL_STATE = {
   username: "",
@@ -17,71 +52,156 @@ const INITIAL_STATE = {
   error: null
 };
 function SignUpFormBase(props) {
-const [userDetails, setUserDetails] = useState({ ...INITIAL_STATE })
+  const classes = useStyles();
+  const [userDetails, setUserDetails] = useState({ ...INITIAL_STATE });
   const onSubmit = event => {
     const { username, email, passwordOne } = userDetails;
     console.log("Signing Up");
-    this.props.firebase
+    props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
-        setUserDetails({ ...INITIAL_STATE });
-        this.props.history.push("/blog/123");
+        setUserDetails({ ...userDetails, ...INITIAL_STATE });
+        props.history.push("/blog/123");
         console.log("Success");
       })
       .catch(error => {
-        setUserDetails({ error });
+        setUserDetails({ ...userDetails, error });
         console.log("Error");
         console.log(error);
       });
     event.preventDefault();
   };
   const onChange = event => {
-    setUserDetails({ [event.target.name]: event.target.value });
+    setUserDetails({...userDetails, [event.target.name]: event.target.value });
   };
-    const { username, email, passwordOne, passwordTwo, error } = userDetails;
+  const { username, email, passwordOne, passwordTwo, error } = userDetails;
 
-    const isInvalid =
-      passwordOne !== passwordTwo ||
-      passwordOne === "" ||
-      email === "" ||
-      username === "";
+  const isInvalid =
+    passwordOne !== passwordTwo ||
+    passwordOne === "" ||
+    email === "" ||
+    username === "";
 
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign Up
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
-    );
+  return (
+    <React.Fragment>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} onSubmit={onSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="username"
+                  variant="outlined"
+                  value={username}
+                  onChange={onChange}
+                  type="text"
+                  placeholder="Full Name"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  value={email}
+                  onChange={onChange}
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  type="text"
+                  placeholder="Email Address"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="passwordOne"
+                  label="Password"
+                  type="password"
+                  value={passwordOne}
+                  onChange={onChange}
+                  id="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="passwordTwo"
+                  label="Confirm Password"
+                  type="password"
+                  value={passwordTwo}
+                  onChange={onChange}
+                  id="password"
+                  placeholder="Confirm Password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              disabled={isInvalid}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            {error && <p>{error.message}</p>}
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
 }
 const SignUpLink = () => (
   <p>
@@ -89,5 +209,5 @@ const SignUpLink = () => (
   </p>
 );
 const SignUpForm = withRouter(withFirebase(SignUpFormBase));
-export default SignUpPage;
-export { SignUpForm, SignUpLink };
+export default SignUpForm;
+export { SignUpLink };
